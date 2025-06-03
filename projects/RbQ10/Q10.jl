67fe26071fbc6fdefc4e26e2bf67843b9b05144b
Lazaro Alonso <lazarus.alon@gmail.com>
2025-06-03 23:17:15 +0200

    added named targets

--- projects/RbQ10/Q10.jl
+++ projects/RbQ10/Q10.jl
@@ -6,24 +6,27 @@ using Random
 using LuxCore
 using CSV, DataFrames
 using EasyHybrid.MLUtils
-
+using EasyHybrid.AxisKeys
+using Zygote
 # data
-df = CSV.read("/Users/lalonso/Documents/HybridML/data/Rh_AliceHolt_forcing_filled.csv", DataFrame)
+df_o = CSV.read("/Users/lalonso/Documents/HybridML/data/Rh_AliceHolt_forcing_filled.csv", DataFrame)
 
+df = copy(df_o)
 df[!, :Temp] = df[!, :Temp] .- 273.15 # convert to Celsius
-df_forcing = filter(:Respiration_heterotrophic => !isnan, df)
+# df = filter(:Respiration_heterotrophic => !isnan, df)
+rename!(df, :Respiration_heterotrophic => :Rh)  # rename as in hybrid model
 df_forcing = df
-ds_k = to_keyedArray(Float32.(df_forcing))
-yobs =  ds_k(:Respiration_heterotrophic)'[:,:]
+
+ds_p_f = to_keyedArray(Float32.(df_forcing)) # predictors + forcing
+ds_t =  ds_p_f([:Rh]) # do the array so that you conserve the name
 
 NN = Lux.Chain(Dense(2, 15, Lux.relu), Dense(15, 15, Lux.relu), Dense(15, 1));
 #? do different initial Q10s
-RbQ10 = RespirationRbQ10(NN, (:Rgpot, :Moist), (:Temp,), 2.5f0) 
+RbQ10 = RespirationRbQ10(NN, (:Rgpot, :Moist), (:Rh, ), (:Temp,), 2.5f0) 
 
 # ? play with :Temp as predictors in NN, temperature sensitivity!
 # TODO: variance effect due to LSTM vs NN
-
-out = train(RbQ10, (ds_k([:Rgpot, :Moist, :Temp]), yobs), (:Q10, ); nepochs=1000, batchsize=512, opt=Adam(0.01));
+out = train(RbQ10, (ds_p_f, ds_t), (:Q10, ); nepochs=1000, batchsize=512, opt=Adam(0.01));
 
 
 with_theme(theme_light()) do 
@@ -39,10 +42,10 @@ with_theme(theme_light()) do
     fig = Figure(; size = (1200, 600))
     ax_train = Makie.Axis(fig[1, 1], title = "training")
     ax_val = Makie.Axis(fig[2, 1], title = "validation")
-    lines!(ax_train, out.ŷ_train[:], color=:orangered, label = "prediction")
+    lines!(ax_train, out.ŷ_train.Rh[:], color=:orangered, label = "prediction")
     lines!(ax_train, out.y_train[:], color=:dodgerblue, label ="observation")
     # validation
-    lines!(ax_val, out.ŷ_val[:], color=:orangered, label = "prediction")
+    lines!(ax_val, out.ŷ_val.Rh[:], color=:orangered, label = "prediction")
     lines!(ax_val, out.y_val[:], color=:dodgerblue, label ="observation")
     axislegend(; position=:lt)
     Label(fig[0,1], "Observations vs predictions", tellwidth=false)
@@ -62,22 +65,20 @@ with_theme(theme_light()) do
 end
 
 
-ds_k = to_keyedArray(Float32.(df))
-yobs_all =  ds_k(:Respiration_heterotrophic)'[:,:]
+yobs_all =  ds_p_f(:Rh)
 
-ŷ, RbQ10_st = LuxCore.apply(RbQ10, ds_k, out.ps, out.st)
+ŷ, RbQ10_st = LuxCore.apply(RbQ10, ds_p_f, out.ps, out.st)
 
 with_theme(theme_light()) do 
     fig = Figure(; size = (1200, 600))
     ax_train = Makie.Axis(fig[1, 1], title = "full time series")
-    lines!(ax_train, ŷ[:], color=:orangered, label = "prediction")
+    lines!(ax_train, ŷ.Rh[:], color=:orangered, label = "prediction")
     lines!(ax_train, yobs_all[:], color=:dodgerblue, label ="observation")
     axislegend(ax_train; position=:lt)
     Label(fig[0,1], "Observations vs predictions", tellwidth=false)
     fig
 end
 
-
 # ? Rb
 lines(out.αst_train.Rb[:])
-lines!(ds_k(:Moist)[:])
+lines!(ds_p_f(:Moist)[:])
