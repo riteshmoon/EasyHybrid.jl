b804d72b3403fc9f6520a5bc8a6777d73cfe6813
Lazaro Alonso <lazarus.alon@gmail.com>
2025-08-11 12:32:06 +0200

    show me

--- src/models/GenericHybridModel.jl
+++ src/models/GenericHybridModel.jl
@@ -427,33 +427,87 @@ function (m::MultiNNHybridModel)(ds_k, ps, st)
 end
 
 # Display functions
-function Base.display(hm::SingleNNHybridModel)
-    println("Neural Network: ", hm.NN)
-    println("Predictors: ", hm.predictors)
-    println("Forcing: ", hm.forcing)
-    println("neural parameters: ", hm.neural_param_names)
-    println("global parameters: ", hm.global_param_names)
-    println("fixed parameters: ", hm.fixed_param_names)
-    println("scale NN outputs: ", hm.scale_nn_outputs)
-    println("parameter defaults and bounds:")
-    display(hm.parameters)
+function print_key_value(io, key, value; color=:light_red)
+    print(io, "$key: ")
+    printstyled(io, value; color=color)
+    println(io)
 end
 
-function Base.display(hm::MultiNNHybridModel)
-    println("Neural Networks:")
+function Base.show(io::IO, pc::ParameterContainer)
+    table = pc.table
+    println(io)
+    PrettyTables.pretty_table(
+        io, table;
+        header     = collect(keys(table.axes[2])),
+        row_labels = collect(keys(table.axes[1])),
+        alignment  = :r,
+    )
+end
+
+function Base.show(io::IO, hm::SingleNNHybridModel)
+    println(io, "Neural Network:")
+    show(IndentedIO(io), MIME"text/plain"(), hm.NN)
+    println(io)
+    
+    print_key_value(io, "Predictors", hm.predictors)
+    print_key_value(io, "Forcing", hm.forcing)
+    print_key_value(io, "Neural parameters", hm.neural_param_names)
+    print_key_value(io, "Global parameters", hm.global_param_names)
+    print_key_value(io, "Fixed parameters", hm.fixed_param_names)
+    print_key_value(io, "Scale NN outputs", hm.scale_nn_outputs)
+    
+    println(io, "Parameter defaults and bounds:")
+    show(IndentedIO(io), MIME"text/plain"(), hm.parameters)
+end
+
+function Base.show(io::IO, hm::MultiNNHybridModel)
+    printstyled(io, "Neural Networks:"; color=:light_yellow)
+    println(io)
     for (name, nn) in pairs(hm.NNs)
-        println("  $name: ", nn)
+        printstyled(io, "$name:\n"; color=:light_black)
+        show(IndentedIO(io), MIME"text/plain"(), nn)
+        println(io)  # add spacing between networks
     end
     
-    println("Predictors:")
+    print(io, "Predictors:")
     for (name, preds) in pairs(hm.predictors)
-        println("  $name: ", preds)
+        printstyled(io, " $name: ", color=:light_black)
+        printstyled(io, preds; color=:light_red)
+        println(io)
     end
-    println("Forcing: ", hm.forcing)
-    println("neural parameters: ", hm.neural_param_names)
-    println("global parameters: ", hm.global_param_names)
-    println("fixed parameters: ", hm.fixed_param_names)
-    println("scale NN outputs: ", hm.scale_nn_outputs)
-    println("parameter defaults and bounds:")
-    display(hm.parameters)
+    
+    print_key_value(io, "Forcing", hm.forcing)
+    print_key_value(io, "Neural parameters", hm.neural_param_names)
+    print_key_value(io, "Global parameters", hm.global_param_names)
+    print_key_value(io, "Fixed parameters", hm.fixed_param_names)
+    print_key_value(io, "Scale NN outputs", hm.scale_nn_outputs)
+    
+    println(io, "Parameter defaults and bounds:")
+    show(IndentedIO(io), MIME"text/plain"(), hm.parameters)
 end
+
+mutable struct IndentedIO{IOType<:IO} <: IO
+    io::IOType
+    indent::String
+    at_line_start::Bool
+end
+
+function IndentedIO(io::IO; indent="    ")
+    IndentedIO{typeof(io)}(io, indent, true)
+end
+
+function Base.write(ido::IndentedIO, data::UInt8)
+    c = Char(data)
+    if ido.at_line_start && c != '\n'
+        write(ido.io, ido.indent)
+        ido.at_line_start = false
+    end
+    write(ido.io, data)
+    ido.at_line_start = (c == '\n')
+    return 1
+end
+
+Base.flush(ido::IndentedIO) = flush(ido.io)
+Base.isopen(ido::IndentedIO) = isopen(ido.io)
+Base.close(ido::IndentedIO) = close(ido.io)
+Base.readavailable(ido::IndentedIO) = readavailable(ido.io)
